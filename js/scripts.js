6be4560089fbf3db0aa43395d26f485aad75ae6e
Otybrian <otieno.brian@student.moringaschool.com>
2022-01-20 01:09:47 +0300

    finish calling function sendMessage

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,22 @@ function sendMessage (){
     var message = document.getElementById("message").value;
 
 
+    if (inputtedName == ""){
+        document.getElementById("error-one").innerHTML='Name cannot be blank!';
+    }
+ 
+    if (email == ""){
+     document.getElementById("error-two").innerHTML='Email cannot be blank!';
+    }
+ 
+    if (message == ""){
+     document.getElementById("error-three").innerHTML='Kindly add a message!';
+    }
+ 
+    else{
+        document.getElementById("success").innerHTML="Hello " + inputtedName + ", your message was sent successfully! We will get back to you on " + email + ". Thanks";
+    }
 
 
-    
+
 }
